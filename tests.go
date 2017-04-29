5fd3e1ebae64bef5fb3fb4819c56839b6fabecc6
Rickard Gustafsson <rickard@allyourbase.se>
2017-04-29 16:23:13 +0200

    Tests, more tests

--- tests.go
+++ tests.go
@@ -1,8 +1,8 @@
-package tests
+package sn
 
 import (
 	"fmt"
-	"github/sn.go"
+	//"github/sn.go"
 )
 
 func test_all() bool {
@@ -29,10 +29,10 @@ func test_all() bool {
 /* Test if authentication works.
  * Should return User struct with email same as inparameter email and nonempty auth
  */
-func test1_get_auth(email string, pass string) (sn.User, bool) {
-	u, err := sn.GetAuth(email, pass)
+func test1_get_auth(email string, pass string) (User, bool) {
+	u, err := GetAuth(email, pass)
 	if err != nil {
-		return sn.User{}, false
+		return User{}, false
 	}
 
 	return u, (u.Email == email && u.Auth != "")
@@ -41,8 +41,8 @@ func test1_get_auth(email string, pass string) (sn.User, bool) {
 /* Test auth string and note creation
  * Create a note with text and tag and check Key and Tags is nonempty
  */
-func test2_create_note(u sn.User) (sn.Note, bool) {
-	var n sn.Note
+func test2_create_note(u User) (Note, bool) {
+	var n Note
 	n.Content = "Test string"
 	n.Tags = []string{"Test_tag"}
 
@@ -54,7 +54,7 @@ func test2_create_note(u sn.User) (sn.Note, bool) {
 /* Test fetching of notes list.
  * Check that list contians a known key and lenght is not zero
  */
-func test3_get_note_list(u sn.User, key string) bool {
+func test3_get_note_list(u User, key string) bool {
 	nl, err := u.GetAllNotes()
 
 	if err != nil {
@@ -72,4 +72,23 @@ func test3_get_note_list(u sn.User, key string) bool {
 	}
 
 	return found && nl.Count != 0
+}
+
+func test4_get_note(u User, key string) (Note, bool) {
+	n, err := u.GetNote(key, 0)
+
+	if err != nil {
+		fmt.Println(err)
+		return n, false
+	}
+
+	return n, n.Content == "Test string"
+}
+
+func test5_update_note(u User, n Note) (Note, bool) {
+    n.Content = "New test string"
+    nn := u.UpdateNote(&n)
+    nn, _ = u.GetNote(nn.Key, 0)
+
+    return nn, nn.Content == "New test string"
 }
